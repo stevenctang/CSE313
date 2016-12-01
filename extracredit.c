addce714590b17596939777664f337748b50ac57
Steven <stevenchhaytang@gmail.com>
2016-11-30 22:41:28 -0800

    change

--- extracredit.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int * random (int seed, int size);
-
-int main(){
-	int array[20];
-	int a;
-	a = random(10,20)
-}
---
+++ xtracredit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int * random (int seed, int size){
+	//return 5;
+}
+int add (int x, int y){
+	int sum;
+	sum = x + y;
+	return sum;
+}
+int main(){
+	int array[20];
+	int a;
+	int sum;
+	sum = add(2,2);
+	printf("%x\n",sum);
+}
